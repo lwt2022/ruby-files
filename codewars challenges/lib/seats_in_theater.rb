4b1e2776f3ca7958b89adb5cc1dce7f42c545952
Antonio Jurado <100658674+lwt2022@users.noreply.github.com>
2022-09-17 16:41:52 +0900

    added new solutions

--- codewars challenges/lib/seats_in_theater.rb
+++ codewars challenges/lib/seats_in_theater.rb
@@ -4,4 +4,21 @@ end
 
 def seats_in_theater a,b,c,d
   (a-c+1).*b-d
-  end
+end
+
+def seats_in_theater(tot_cols, tot_rows, col, row)
+  c = tot_cols - col + 1
+  r = tot_rows - row
+  c*r
+end
+
+
+def seats_in_theater(tot_cols, tot_rows, col, row)
+  columnsLeftMe = tot_cols - col + 1
+  rowsBehindMe = tot_rows - row
+  columnsLeftMe * rowsBehindMe
+end
+
+def seats_in_theater(tot_cols, tot_rows, col, row)
+  (tot_rows-row)*(tot_cols- col+1)
+end
