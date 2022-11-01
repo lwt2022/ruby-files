cb2cec37b9a5fa534def1f2cb2edd93421c832c8
antonio jurado <iberico.cortador@gmail.com>
2022-11-01 17:01:34 +0900

    basic operator

--- codewars challenges/lib/seats_in_theater.rb
+++ codewars challenges/lib/seats_in_theater.rb
@@ -5,3 +5,23 @@ end
 def seats_in_theater a,b,c,d
   (a-c+1).*b-d
   end
+
+  def seats_in_theater(tot_cols, tot_rows, col, row)
+    (col..tot_cols).count*(row...tot_rows).count
+  end
+
+  def seats_in_theater(tot_cols, tot_rows, col, row)
+    columnsLeftMe = tot_cols - col + 1
+    rowsBehindMe = tot_rows - row
+    columnsLeftMe * rowsBehindMe
+  end
+
+  def seats_in_theater(nCols, nRows, col, row)
+    (nCols-col+1) * (nRows - row)
+  end
+
+  def seats_in_theater(tot_cols, tot_rows, col, row)
+    c = tot_cols - col + 1
+    r = tot_rows - row
+    c*r
+  end
